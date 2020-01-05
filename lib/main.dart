bc0aaa9cb77beabd343eb14b81e618071a763a5b
Rifat <mohd.asfaqeazam@gmail.com>
2020-01-06 02:24:53 +0600

    1. Upgrade target sdk version, 2. Add implementation of random image changing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,39 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+  MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask me anything'),
+      ),
+      body: BallPage(),
+    ),
+  ),
+);
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int _ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            _ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$_ballNumber.png'),
       ),
     );
+  }
+}
